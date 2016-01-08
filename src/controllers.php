41846ada432008bca502d283d86239928f4256b8
Diego Brocanelli <diegod2@msn.com>
2016-01-08 00:49:32 -0200

    commit inicial do projeto.

--- src/controllers.php
+++ src/controllers.php
@@ -6,13 +6,34 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
-//Request::setTrustedProxies(array('127.0.0.1'));
+//config DB
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+    'dbs.options' => array (
+        'mysql_read' => array(
+            'driver'    => 'pdo_mysql',
+            'host'      => 'localhost',
+            'dbname'    => 'beacons',
+            'user'      => 'root',
+            'password'  => 'root',
+            'charset'   => 'utf8mb4',
+        )
+    ),
+));
 
-$app->get('/', function () use ($app) {
-    return $app['twig']->render('index.html', array());
-})
-->bind('homepage')
-;
+$app->get('/{token}/{empresa}', function ($token, $empresa) use ($app) {
+    (int)$idEmpresa = $empresa;
+
+    $sql = 'SELECT * FROM produtos WHERE id_empresa = ?';
+    try{
+        $query = $app['dbs']['mysql_read']->fetchAssoc($sql, array($idEmpresa));
+    }catch(\Exceptnio $e){
+        return new Response(500);
+    }
+
+    $response = new JsonResponse($query);
+
+    return $response;
+});
 
 $app->error(function (\Exception $e, $code) use ($app) {
     if ($app['debug']) {
